29f14ea7e634e7ac3b7b79809ab35b4d5b77d043
Tiago Pereira <targpereira@gmail.com>
2021-05-01 12:36:12 +0100

    Initial Push

---
+++ tests/test_lib.py
@@ -0,0 +1,14 @@
+from lib import soma,diff,mult,div
+
+def test_soma():
+    assert soma(1,1) == 2
+
+def test_diff():
+    assert diff(1,1) == 0
+
+def test_mult():
+    assert mult(3,3) == 9
+
+def test_div():
+    assert div(20,4) == 5
+
